bdac6f83ee272ad4c19a3da1725785f51d51a141
AslanHattukai96 <133890962+AslanHattukai96@users.noreply.github.com>
2024-05-06 20:55:23 +0200

    edir false turkish

--- pages/Turkish.py
+++ pages/Turkish.py
@@ -8,11 +8,11 @@ df = pd.read_csv('words.csv')
 
 # Set page title and header
 st.markdown("<h1 style='text-align: center; color: yellow;'>⋰Ẍ⋱</h1>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center; color: yellow;'>למידת אדיג'בזה</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: yellow;'>Adığece Öğren</h1>", unsafe_allow_html=True)
 st.title("")
 
-# Set target language to Hebrew
-target_language = 'עברית'
+# Set target language to Turkish
+target_language = 'Türkçe'
 
 # Initialize or update previous language selection
 if 'prev_language' not in st.session_state:
@@ -60,19 +60,19 @@ if 'circassian_word' not in st.session_state or 'correct_translation' not in st.
     st.session_state.next_button_clicked = False
 
 if st.session_state.circassian_word is not None:
-    st.write(f"תרגם את המילה הבאה מצ'רקסית לעברית: **{st.session_state.circassian_word}**")
+    st.write(f"Aşağıdaki kelimeyi Çerkesçeden Türkçeye çevirin: **{st.session_state.circassian_word}**")
 
-    user_translation = st.text_input(f"התרגום שלך: ").strip()
+    user_translation = st.text_input(f"Türkçe çeviriniz: ").strip()
     
     # Change button label according to the target language
-    submit_button_label = "בדוק"  # Change button label to "בדוק"
+    submit_button_label = "Kontrol Et"  # Change button label to "Kontrol Et"
     
     submit_button = st.button(submit_button_label)
 
     # Check if the user's translation is correct or incorrect, only when submit button is pressed
     if submit_button and user_translation:
         if user_translation.lower() == st.session_state.correct_translation.lower():
-            st.success("התרגום נכון!")
+            st.success("Doğru çeviri!")
             st.session_state.correct_count += 1  # Increment correct count
             st.session_state.guessed_words.append(st.session_state.circassian_word)  # Add guessed word to list
             set_new_word()  # Set a new word and translation
@@ -89,9 +89,9 @@ if st.session_state.circassian_word is not None:
             time.sleep(3)  # Wait for 3 seconds
             st.experimental_rerun()
         else:
-            st.error(f"התרגום שגוי. התרגום הנכון היה: **{st.session_state.correct_translation}**")
+            st.error(f"Yanlış çeviri. Doğru çeviri: **{st.session_state.correct_translation}**")
 
-    st.write(f"סך הכל נסיונות נכונים: {st.session_state.correct_count}")
+    st.write(f"Toplam Doğru Deneme Sayısı: {st.session_state.correct_count}")
 else:
-    st.write("🎈 סיימת!")
+    st.write("🎈 Tamamlandı!")
     st.image("succes.jpeg")
